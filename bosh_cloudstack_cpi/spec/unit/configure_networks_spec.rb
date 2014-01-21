15fce2b2d7353bcbc1189b3d8e61e294af1d25d8
m1093782566 <dj199008@gmail.com>
2014-01-21 19:06:07 +0800

    add test case "add floating ip to the server for vip network"

--- bosh_cloudstack_cpi/spec/unit/configure_networks_spec.rb
+++ bosh_cloudstack_cpi/spec/unit/configure_networks_spec.rb
@@ -147,7 +147,37 @@ describe Bosh::CloudStackCloud::Cloud do
   end
 
   it "adds floating ip to the server for vip network" do
-    # TODO
+    address = double("address", :id => "i-test", :virtual_machine_id => nil)
+    server = double("server", :id => "i-test", :name => "i-test", :addresses => [address], :zone_id => 'foobar-2a')
+    security_group = double("security_groups", :name => "default")
+
+    server.should_receive(:security_groups).and_return([security_group])
+    address.should_receive(:ip_address).and_return("10.10.10.1")
+    server.should_receive(:nics).and_return([{'id' => 'i-test', 'networkid' => 'i-test'}])
+
+    nat_params={
+        :ip_address_id => address.id,
+        :virtual_machine_id => server.id,
+        :network_id => "i-test",
+    }
+
+    nat = double("nat")
+    nat.should_receive(:enable)
+
+    cloud = mock_cloud do |compute|
+      compute.servers.should_receive(:get).with("i-test").and_return(server)
+      compute.zones.should_receive(:get).with("foobar-2a").and_return(compute.zones[1])
+      compute.ipaddresses.should_receive(:find).and_return(address)
+      compute.nats.should_receive(:new).with(nat_params).and_return(nat)
+    end
+
+    old_settings = { "foo" => "bar", "networks" => { "network_a" => dynamic_network_spec } }
+    new_settings = { "foo" => "bar", "networks" => combined_network_spec }
+
+    @registry.should_receive(:read_settings).with("i-test").and_return(old_settings)
+    @registry.should_receive(:update_settings).with("i-test", new_settings)
+
+    cloud.configure_networks("i-test", combined_network_spec)
   end
 
   it "removes floating ip from the server if vip network is gone" do
